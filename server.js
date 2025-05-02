864461b05209e71b6afa1914415b130d985a956e
illegaljarvis <108478153+illegaljarvis@users.noreply.github.com>
2025-05-02 17:28:23 +0530

    Comma replaced with dot

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use('/api/v1/transactions', require('./routes/transactionRoutes'))
 //static files
 app.use(express.static(path.join(__dirname, './client/build')))
 
-app,get('*', function(req,res){
+app.get('*', function(req,res){
     res.sendFile(path.join(__dirname,'./client/build/index.html'));
 })
 
